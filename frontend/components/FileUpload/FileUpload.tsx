d128a2a401d395debe82c191bba7da4415030945
John Trinh <johntrinhcode@gmail.com>
2021-10-17 23:29:34 -0400

    mswitching files over to typescript and some progress with file upload on the frontend side

---
+++ frontend/components/FileUpload/FileUpload.tsx
@@ -0,0 +1,60 @@
+import { FiUpload } from 'react-icons/fi';
+import { DragEvent, useState, ChangeEvent } from 'react';
+
+export default function FileUpload() {
+  const [fileName, setFileName] = useState(null);
+
+  const fileUploadHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setFileName(event.target.files[0].name)
+  };
+
+  const dragOverHandler = (event: DragEvent<HTMLLabelElement>) => { 
+    // Prevent default behavior (Prevent file from being opened)
+    event.stopPropagation();
+    event.preventDefault();
+   }
+
+  const dropHandler = (event: DragEvent<HTMLLabelElement>) => { 
+    // Prevent default behavior (Prevent file from being opened)
+    event.preventDefault();
+    if (event.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (let i = 0; i < event.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (event.dataTransfer.items[i].kind === 'file') {
+          const file = event.dataTransfer.items[i].getAsFile();
+          console.log('... file[' + i + '].name = ' + file.name);
+          setFileName(file.name);
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (let i = 0; i < event.dataTransfer.files.length; i++) {
+        console.log('... file[' + i + '].name = ' + event.dataTransfer.files[i].name);
+      }
+    }
+  }
+
+  return (
+    <section className="flex flex-col pt-8 px-8 pb-16 rounded-md">
+      <span className="text-4xl font-bold text-gray-700">Upload</span>
+      <span className="text-gray-400 mt-2">Give us a .xls file to process. Don’t have one? Download a template 
+      <a className="text-indigo-400 font-bold cursor-pointer hover:text-indigo-500"> here</a>.</span>
+      <div id="file-input" className="flex flex-row h-14 space-x-4 mt-8 justify-center">
+        <label id="drop-zone" htmlFor="file-upload" onDrop={ dropHandler } onDragOver={ dragOverHandler } className="flex w-96 bg-indigo-50 hover:bg-indigo-100 cursor-pointer transition-all duration-200 rounded-md border-dashed border border-indigo-400 items-center justify-center">
+          <span className="text-indigo-400">
+            {
+              (fileName) ? fileName : 'Drag & drop your file or click here'
+            }
+          </span>
+        </label>
+        <button className={ "flex h-full w-14 rounded-md text-white justify-center items-center  transition-all duration-200 " + (fileName ? 'bg-indigo-400 hover:bg-indigo-500' : 'bg-gray-300 cursor-not-allowed')}>
+          <FiUpload className="h-6 w-6"/>
+        </button>
+      </div>
+      <input type="file" id="file-upload" className="w-0 absolute invisible" onChange={ fileUploadHandler }/>
+    </section>
+  );
+
+
+}
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+import FileUpload from '../components/FileUpload/FileUpload';
+
+export default function Home() {
+
+  
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Trendly</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <FileUpload />
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
